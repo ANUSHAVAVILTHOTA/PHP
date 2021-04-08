c85fd0a2c440c2b0b6d0a7a88da170647cbe1b1f
Anusha V <43380712+ANUSHAVAVILTHOTA@users.noreply.github.com>
2021-04-08 22:32:40 +0530

    08-04-21

---
+++ 08-04-21/basics.php
@@ -0,0 +1,3 @@
+<?php
+   echo "My First PHP SCRIPT  ABC XYZ";
+?>
---
+++ 08-04-21/kp.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+     Welcome to php 
+     <h1 style="background-color: green;">sdfbjds</h1>
+     <p>eoudbsjn udbksje</p>
+     iwdhl
+
+     <em>sajkdvbhs</em>
+
+    <!--  document.write("<br />")
+     alert()
+     console.log()
+
+
+///do display content on the browser -->
+
+
+     <?php
+
+         echo "My First PHP SCRIPT  ABC XYZ";
+         ECHO "My First PHP SCRIPT  ABC XYZ";
+         EcHo "My First PHP SCRIPT  ABC XYZ";
+
+         echo "<br />";
+
+
+// always CAR car  Car  are different in variable names
+         $color="ABCDEFG";  //php will consider below 3 are different variables
+         $COLOR="XYZ";
+         $Color="PADMD"; //string
+         $a=10;  //integer
+         $b=18.6;  //float
+
+        
+
+
+         echo $color;  
+
+         echo "<br />";
+
+         echo $COLOR;
+
+         echo "<br />";
+
+         echo $Color;
+
+         echo "<br />";
+
+         echo $a;
+
+         echo "<br />";
+
+         echo $b;
+
+        echo "<br />";
+
+         $c=$a+$b;
+
+         echo $c;
+
+        echo "<br />";
+
+
+        $text="KScldsn m, sdlnds";  //string
+
+        echo "gggggggbgggnklhkjjk".$text."SADKyfcgbyfkhvcyfucgjnb tfujbmkh";
+
+        $k=true;   //boolean
+
+        $l=false;  //boolean
+
+        $h=null;   //null
+
+        //in php using . we can add the content  (concat)
+
+//string  "ABC"
+ //integer  12   34   56   789  89786
+  //float    7.89   3.45   12.34
+  //boolean    true false
+
+  //array     array("ABC","DSF","SDFC",10,10.78,true,null)  //all data types
+
+  //null     null
+
+
+
+
+         
+         //echo to display output or just to display the content
+
+     ?>
+
+
+ <!-- 
+
+Download XAampp
+install exe file
+start server database
+
+
+localdisk ---xampp-htdocs ----files and folders
+
+
+localhost:80/foldername/filename.php
+  -->
+</body>
+</html>
